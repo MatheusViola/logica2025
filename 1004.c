98614f9970ed7f18b32b5f45dc3902ab1f9f53e4
MatheusViola <viola.matheus09@unifesp.br>
2025-06-25 10:54:54 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, PROD;
+
+    // Leitura dos valores inteiros
+    scanf("%d %d", &A, &B);
+
+    // Cálculo do produto
+    PROD = A * B;
+
+    // Impressão do resultado
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
