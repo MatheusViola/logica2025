a150f7bd8151dbee622572ac7706206559565972
MatheusViola <viola.matheus09@unifesp.br>
2025-06-25 10:57:55 -0300

    Update 1003.c

--- 1003.c
+++ 1003.c
@@ -3,13 +3,10 @@
 int main() {
     int A, B, SOMA;
 
-    // Leitura dos valores inteiros
     scanf("%d %d", &A, &B);
 
-    // Cálculo da soma
     SOMA = A + B;
 
-    // Impressão do resultado
     printf("SOMA = %d\n", SOMA);
 
     return 0;
