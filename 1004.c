70216731d268929da08e5d4e36636f1dcebf7e51
MatheusViola <viola.matheus09@unifesp.br>
2025-06-25 10:57:10 -0300

    Update 1004.c

--- 1004.c
+++ 1004.c
@@ -3,13 +3,10 @@
 int main() {
     int A, B, PROD;
 
-    // Leitura dos valores inteiros
     scanf("%d %d", &A, &B);
 
-    // Cálculo do produto
     PROD = A * B;
 
-    // Impressão do resultado
     printf("PROD = %d\n", PROD);
 
     return 0;
