9fe46bf08004e4c73cce097edf0615c3b36b09b0
MatheusViola <viola.matheus09@unifesp.br>
2025-06-25 10:52:52 -0300

    Create 1003.c

---
+++ 1003.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, SOMA;
+
+    // Leitura dos valores inteiros
+    scanf("%d %d", &A, &B);
+
+    // Cálculo da soma
+    SOMA = A + B;
+
+    // Impressão do resultado
+    printf("SOMA = %d\n", SOMA);
+
+    return 0;
+}
