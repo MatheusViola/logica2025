3926c4b54294943551a1d776e9d297f4be046071
MatheusViola <viola.matheus09@unifesp.br>
2025-06-25 11:05:59 -0300

    Create 1009.c

---
+++ 1009.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char nome[50];
+    double salario, vendas, total;
+    scanf("%s %lf %lf", nome, &salario, &vendas);
+    total = salario + (vendas * 0.15);
+    printf("TOTAL = R$ %.2lf\n", total);
+    return 0;
+}
