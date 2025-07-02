961fe17d965e0f82783ba7256a5b0950629926d9
MatheusViola <viola.matheus09@unifesp.br>
2025-07-02 09:21:12 -0300

    Update 1383.cc

--- 1383.cc
+++ 1383.cc
@@ -1,61 +1,54 @@
-#include <iostream>
-#include <vector>
-#include <set>
+#include <stdio.h>
 
-using namespace std;
+int checkSudoku(int grid[9][9]) {
+    int i, j, k, l;
+    int seen[10];
 
-bool contemNumerosDe1a9(const vector<int>& v) {
-    set<int> s(v.begin(), v.end());
-    if (s.size() != 9) return false;
-    for (int i = 1; i <= 9; ++i)
-        if (s.count(i) == 0) return false;
-    return true;
-}
-
-bool verificarSudoku(const vector<vector<int>>& matriz) {
-    for (int i = 0; i < 9; ++i)
-        if (!contemNumerosDe1a9(matriz[i]))
-            return false;
-
-    for (int j = 0; j < 9; ++j) {
-        vector<int> coluna;
-        for (int i = 0; i < 9; ++i)
-            coluna.push_back(matriz[i][j]);
-        if (!contemNumerosDe1a9(coluna))
-            return false;
-    }
-
-    for (int bloco_i = 0; bloco_i < 3; ++bloco_i) {
-        for (int bloco_j = 0; bloco_j < 3; ++bloco_j) {
-            vector<int> bloco;
-            for (int i = 0; i < 3; ++i)
-                for (int j = 0; j < 3; ++j)
-                    bloco.push_back(matriz[bloco_i * 3 + i][bloco_j * 3 + j]);
-            if (!contemNumerosDe1a9(bloco))
-                return false;
+    for (i = 0; i < 9; i++) {
+        for (k = 1; k <= 9; k++) seen[k] = 0;
+        for (j = 0; j < 9; j++) {
+            int num = grid[i][j];
+            if (num < 1 || num > 9 || seen[num]) return 0;
+            seen[num] = 1;
         }
     }
 
-    return true;
+    for (j = 0; j < 9; j++) {
+        for (k = 1; k <= 9; k++) seen[k] = 0;
+        for (i = 0; i < 9; i++) {
+            int num = grid[i][j];
+            if (num < 1 || num > 9 || seen[num]) return 0;
+            seen[num] = 1;
+        }
+    }
+
+    for (i = 0; i < 9; i += 3) {
+        for (j = 0; j < 9; j += 3) {
+            for (k = 1; k <= 9; k++) seen[k] = 0;
+            for (k = 0; k < 3; k++) {
+                for (l = 0; l < 3; l++) {
+                    int num = grid[i + k][j + l];
+                    if (num < 1 || num > 9 || seen[num]) return 0;
+                    seen[num] = 1;
+                }
+            }
+        }
+    }
+
+    return 1;
 }
 
 int main() {
-    int n;
-    cin >> n;
+    int grid[9][9];
 
-    for (int instancia = 1; instancia <= n; ++instancia) {
-        vector<vector<int>> matriz(9, vector<int>(9));
-        for (int i = 0; i < 9; ++i)
-            for (int j = 0; j < 9; ++j)
-                cin >> matriz[i][j];
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+            scanf("%d", &grid[i][j]);
 
-        cout << "Instancia " << instancia << endl;
-        if (verificarSudoku(matriz))
-            cout << "SIM" << endl;
-        else
-            cout << "NAO" << endl;
-        cout << endl;
-    }
+    if (checkSudoku(grid))
+        printf("Instancia 1\nSIM\n");
+    else
+        printf("Instancia 1\nNAO\n");
 
     return 0;
 }
